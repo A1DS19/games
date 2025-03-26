f33ae335404f8885d07d1d535308478cc670bd5d
Jose Padilla <59151555+A1DS19@users.noreply.github.com>
2025-03-26 10:32:40 -0700

    arrays

---
+++ arrays/main.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  int array[1] = {1};
+
+  for (size_t i = 0; i < 1; i++) {
+    cout << array[i] << endl;
+  }
+
+  return EXIT_SUCCESS;
+}
--- create_project/generate-cpp-project.sh
+++ create_project/generate-cpp-project.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
 
-# Check if a directory name is provided
 if [ -z "$1" ]; then
   echo "Usage: $0 <directory-name>"
   exit 1
 fi
 
-# Create the directory structure
-mkdir -p "$1"/bin
-
-# Navigate into the directory
+mkdir -p "$1/bin"
 cd "$1"
 
-# Create Makefile
-cat > Makefile << EOF
+# Ensure TAB character is preserved in Makefile
+cat > Makefile <<- 'EOF'
 all:
-\tg++ main.cpp -o ./bin/main
+	g++ main.cpp -o ./bin/main
 EOF
 
-cat > .gitignore << EOF
-/*/bin/*
-EOF
-
-# Create main.cpp
-cat > main.cpp << EOF
+cat > main.cpp << 'EOF'
 #include <cstdlib>
 #include <iostream>
 using namespace std;
@@ -35,4 +26,3 @@ int main(int argc, char *argv[]) {
 EOF
 
 echo "Project '$1' created successfully."
-
