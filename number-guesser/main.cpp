37e197b5bd9086e86a7b5ce7ef6ba638644ce6d9
Jose Padilla <59151555+A1DS19@users.noreply.github.com>
2025-03-26 08:11:36 -0700

    number guesser

--- number-guesser/main.cpp
+++ number-guesser/main.cpp
@@ -1,8 +1,53 @@
+#include <cmath>
+#include <cstddef>
 #include <cstdlib>
+#include <ctime>
+#include <ios>
 #include <iostream>
 using namespace std;
 
+const int UPPER_BOUND = 100;
+
+int generateRandomNumber() {
+  srand((unsigned int)time(NULL));
+  return rand() % UPPER_BOUND;
+}
+
 int main(int argc, char *argv[]) {
-  cout << "hello world" << endl;
+  bool playGame = true;
+  int secretNumber = generateRandomNumber();
+  int numberTries = ceil(log2(UPPER_BOUND));
+  int tries = 0;
+  int guess = 0;
+
+  do {
+    if (tries == numberTries) {
+      cout << "Number of tries reached" << endl;
+      break;
+    }
+
+    cout << "Insert number:" << endl;
+    cin >> guess;
+    tries += 1;
+
+    if (cin.fail()) {
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      cout << "Invalid input, try again" << endl;
+      continue;
+    }
+
+    if (guess == secretNumber) {
+      cout << "You guessed correctly" << endl;
+      playGame = false;
+    } else {
+      cout << "Incorrect guess, try again" << endl;
+      continue;
+    }
+
+  } while (playGame);
+
+  cout << "Secret number " << secretNumber << endl;
+
   return EXIT_SUCCESS;
 }
